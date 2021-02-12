90a906ec088e6489d9acf9a3e02f2e02b8a2557d
Georgkali <georgijs.kalicava@gmail.com>
2021-02-12 13:58:45 +0200

    almost done

--- pole.js
+++ pole.js
@@ -1,100 +1,49 @@
-
-
-
-let coaches = document.getElementById('coaches');
-let infoBlog = document.getElementById('info');
-let about = document.getElementById('about');
-let classes = document.getElementById('classes');
-let contacts = document.getElementById('contacts');
-
-
-// coaches button
-
-const showCoaches = () => {
-    coaches.style.color = 'yellow';
-    about.style.color = 'white';
-    classes.style.color = 'white';
-    contacts.style.color = 'white';
-    
-};
-
-coaches.addEventListener('click', showCoaches);
-// about button
-const showAbout = () => {
-
-    about.style.color = 'yellow';
-    coaches.style.color = 'white';
-    classes.style.color = 'white';
-    contacts.style.color = 'white';
-    
-};
-
-about.addEventListener('click', showAbout);
-// classes button
-const showClasses = () => {
-    
-    classes.style.color = 'yellow';
-    coaches.style.color = 'white';
-    about.style.color = 'white';
-    contacts.style.color = 'white';
-    
-};
-
-classes.addEventListener('click', showClasses);
-// contacts button
-
-const showContacts = () => {
-
-    classes.style.color = 'white';
-    coaches.style.color = 'white';
-    about.style.color = 'white';
-    contacts.style.color = 'yellow';
-    
-};
-
-contacts.addEventListener('click', showContacts);
-
-// conatcts section
-
-
-
-
-
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-  let points = document.getElementsByClassName("point");
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+function navButtonHighlight(n) {
+  let navButton = document.getElementsByClassName("nav_button");
+  navButton[n].style.color = "yellow";
+  for(i = 0; i < navButton.length; i++) {
+    if(i != n) {
+    navButton[i].style.color = "white";
     }
-    for (i = 0; i < points.length; i++) {
-      points[i].className = points[i].className.replace(" active", "");
-    }
-  slides[slideIndex-1].style.display = "block";
-  points[slideIndex-1].className += " active";
+  }
 }
 
 
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  const uluru = { lat: 56.993766, lng: 24.187939 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
+
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let points = document.getElementsByClassName("point");
+    if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < points.length; i++) {
+        points[i].className = points[i].className.replace(" active", "");
+      }
+    slides[slideIndex-1].style.display = "block";
+    points[slideIndex-1].className += " active";
+  }
+
+
+  // Initialize and add the map
+  function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 56.993766, lng: 24.187939 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 14,
     center: uluru,
   });
